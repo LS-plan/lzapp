5ad41fd1a26a6c4901a37f5cc2517eed11fec353
LS-plan <2016040219@s.upc.edu.cn>
2023-07-11 13:10:13 +0800

    修改了运行端口，补充了Readme

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def shibie():
     return baidu.tunn()["words_result"]["number"]
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='127.0.0.1', port=5002)
